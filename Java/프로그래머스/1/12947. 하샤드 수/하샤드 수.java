de7648364bdf65aa60cd85babcf2f5669279dd3e
jangutae <jsilll101389@gmail.com>
2024-12-13 09:28:13 +0900

    [level 1] Title: 하샤드 수, Time: 2.57 ms, Memory: 88.1 MB -BaekjoonHub

--- Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        int sum = 0;
-        
-        char[] chars = String.valueOf(x).toCharArray();
-        
-        for (char c : chars) {
-            sum += Character.getNumericValue(c);
-            answer = ( x % sum == 0) ? true : false;
-        }
-            
-        System.out.println(answer);
         
         
-        return answer;
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();;
+        
+      
+        System.out.println(x % sum == 0);
+        
+        
+        return x % sum == 0;
     }
 }
