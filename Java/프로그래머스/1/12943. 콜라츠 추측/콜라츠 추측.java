b812d1bbbd9b7f5c861ccd7fc1ee90cde0655578
jangutae <jsilll101389@gmail.com>
2024-12-16 10:11:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 81.9 MB -BaekjoonHub

--- Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,26 +1,21 @@
 class Solution {
-    public long solution(long num) {
-        long answer = 0;
-        long count = 0;
+    public int solution(int num) {
+        int answer = 0;
+        long n = (long) num;
         
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-                count++;
-                if (count > 500) {
-                    count = -1;
-                    break;
-                }  
-            } else if (num % 2 != 0) {
-                num = num * 3 + 1;
-                count++;
-            } 
+        while (n != 1) {
+            if (answer == 500) {
+                answer = -1;
+                break;
+            }
+            if (n % 2 == 0) {
+                n /= 2;
+                answer++;
+            } else {
+                n = n * 3 +1;
+                answer++;
+            }
         }
-        answer = count;
-        
-        System.out.println(num);
-        System.out.println(count);
-        System.out.println(answer);
         return answer;
     }
 }
