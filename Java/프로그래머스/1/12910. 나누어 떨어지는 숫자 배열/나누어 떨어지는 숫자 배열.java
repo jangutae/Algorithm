e331412683b4fa9e2bb736bbd6a856c40e7c8da0
jangutae <jsilll101389@gmail.com>
2024-12-16 14:42:40 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 6.00 ms, Memory: 78 MB -BaekjoonHub

--- Java/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ Java/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,34 +1,11 @@
 import java.util.Arrays;
 
+
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        int index = 0;
-        int index1 = 0;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) {
-                index++;
-            }
-        }
-        
-        System.out.println(index);
-        
-        int[] intArr = new int[index];
-        
-        System.out.println(Arrays.toString(intArr));
-        
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[j] % divisor == 0) {
-                intArr[index1] = arr[j];
-                index1++;
-            } 
-        }
-        
-        if (intArr.length == 0) {
-            intArr = new int[]{-1};
-        }
-        Arrays.sort(intArr);
-        System.out.println(Arrays.toString(intArr));
-        return intArr;
+        int[] answer = Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray();
+          if(answer.length == 0) answer = new int[] {-1};
+          Arrays.sort(answer);
+          return answer;
     }
 }
