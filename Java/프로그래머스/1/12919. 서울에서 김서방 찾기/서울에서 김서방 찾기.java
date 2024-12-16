001f85d21e787d805c1b91e8a87976f511925d76
jangutae <jsilll101389@gmail.com>
2024-12-16 10:31:25 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.40 ms, Memory: 84.4 MB -BaekjoonHub

--- Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
-        // 1 <= seoul.length <= 1000
-        // 1 <= seoul[0].length <= 20
+        int count = 0;
+        
         for (int i = 0; i < seoul.length; i++) {
             if (seoul[i].equals("Kim")) {
-                answer = "김서방은 " + i +"에 있다";
-                    break;
+                answer = "김서방은 " + i + "에 있다";
+                break;
             }
         }
         return answer;
