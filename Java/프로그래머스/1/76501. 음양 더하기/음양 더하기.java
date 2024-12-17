393a62e87429bf67430b19a09e93926c210b7bce
jangutae <jsilll101389@gmail.com>
2024-12-17 09:13:07 +0900

    [level 1] Title: 음양 더하기, Time: 0.26 ms, Memory: 77.1 MB -BaekjoonHub

--- Java/프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ Java/프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -3,11 +3,7 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < absolutes.length; i++) {
-            if (signs[i] == true) {
-                answer += absolutes[i];
-            } else {
-                answer -= absolutes[i];
-            }
+           answer += signs[i] ? absolutes[i] : absolutes[i] * -1;
         }
         
         System.out.println(answer);
