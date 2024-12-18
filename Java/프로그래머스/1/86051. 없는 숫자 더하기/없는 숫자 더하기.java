3c1affa2de0d8419a6e9d1a3eac8384de8e6bb51
jangutae <jsilll101389@gmail.com>
2024-12-18 09:27:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.19 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int sum = 0;
+        
+        for (int i = 0; i < numbers.length; i++) {
+                sum += numbers[i];
+        }
+        
+        answer = 45 - sum;
+        
+        
+        System.out.println(answer);
+        return answer;
+    }
+}
