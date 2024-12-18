4d64011701b7e39c0c3293a76434ae23fc2eceb6
jangutae <jsilll101389@gmail.com>
2024-12-18 12:02:39 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 87.7 MB -BaekjoonHub

--- Java/프로그래머스/1/12903. 가운데 글자 가져오기/README.md
+++ Java/프로그래머스/1/12903. 가운데 글자 가져오기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가운데 글자 가져오기 - 12903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903#) 
 
 ### 성능 요약
 
-메모리: 84.6 MB, 시간: 1.30 ms
+메모리: 87.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 11:48:13
+2024년 12월 18일 12:02:37
 
 ### 문제 설명
 
--- Java/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ Java/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,16 +1,16 @@
 class Solution {
-    public String solution(String s) {
-        String answer = "";
+    public String solution(String word) {
+//         String answer = "";
         
-        char[] chars = s.toCharArray();
         
-        if (chars.length % 2 == 0) {
-            answer = String.valueOf(chars[chars.length / 2 - 1]) + String.valueOf(chars[chars.length / 2]);
-        } else {
-            answer = String.valueOf(chars[chars.length / 2]);
-        }
-      
-        System.out.println(answer);
-        return answer;
+//         String[] str = s.split("");
+        
+//         if (str.length % 2 == 0) {
+//             answer = str[str.length / 2 - 1]  + str[str.length / 2];
+//         } else {
+//             answer = str[str.length / 2];
+//         }
+        return word.substring((word.length()-1)/2, word.length()/2 + 1);
+        // return answer;
     }
 }
