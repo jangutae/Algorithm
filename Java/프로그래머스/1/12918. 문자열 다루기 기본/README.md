493c6430222bbdb9f49a405b47d948f4078b54c8
jangutae <jsilll101389@gmail.com>
2024-12-19 22:18:14 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.59 ms, Memory: 78.6 MB -BaekjoonHub

--- Java/프로그래머스/1/12918. 문자열 다루기 기본/README.md
+++ Java/프로그래머스/1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
 
 ### 성능 요약
 
-메모리: 88.6 MB, 시간: 1.07 ms
+메모리: 78.6 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 17:45:53
+2024년 12월 19일 22:18:12
 
 ### 문제 설명
 
--- Java/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ Java/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -5,18 +5,17 @@ class Solution {
         boolean answer = true;
         
         if (s.length() != 4 && s.length() != 6) {
-            return false;
+            answer = false;
         }
         
         char[] ch = s.toCharArray();
+        
         Arrays.sort(ch);
         
         if (!Character.isDigit(ch[ch.length - 1])) {
-            answer = false;  // 숫자가 아니면 false
-        } 
-        
-        System.out.println(Arrays.toString(ch));
-        
+            answer = false;
+        }
+                    
         return answer;
     }
 }
