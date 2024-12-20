597506edd6723370ed3f08b5229d07741602b24c
jangutae <jsilll101389@gmail.com>
2024-12-20 13:08:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.50 ms, Memory: 74.6 MB -BaekjoonHub

--- Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -2,25 +2,34 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String[] strs = s.split(" ", -1);
-        StringBuilder sb = new StringBuilder();
+//         String[] strs = s.split(" ", -1);
+//         StringBuilder sb = new StringBuilder();
        
-        for (int i = 0; i < strs.length; i++) {
-            for (int j = 0; j < strs[i].length(); j++) {
-                if (j % 2 == 0) {
-                    sb.append(Character.toUpperCase(strs[i].charAt(j)));    
-                } else {
-                    sb.append(Character.toLowerCase(strs[i].charAt(j)));
-                }
-            }
-            if (i == strs.length - 1) break;
-            sb.append(" ");
+//         for (int i = 0; i < strs.length; i++) {
+//             for (int j = 0; j < strs[i].length(); j++) {
+//                 if (j % 2 == 0) {
+//                     sb.append(Character.toUpperCase(strs[i].charAt(j)));    
+//                 } else {
+//                     sb.append(Character.toLowerCase(strs[i].charAt(j)));
+//                 }
+//             }
+//             if (i == strs.length - 1) break;
+//             sb.append(" ");
+//         }
+//          System.out.println(sb.toString());
+        
+        
+       
+        
+//         return sb.toString();
+         String answer = "";
+        int cnt = 0;
+        String[] array = s.split("");
+
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
         }
-         System.out.println(sb.toString());
-        
-        
-       
-        
-        return sb.toString();
+      return answer;
     }
 }
