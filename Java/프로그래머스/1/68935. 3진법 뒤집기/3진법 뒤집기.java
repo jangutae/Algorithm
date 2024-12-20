a49abe9d8381221077388d4d96c30d6524099784
jangutae <jsilll101389@gmail.com>
2024-12-20 11:27:56 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ Java/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,15 +2,14 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        StringBuilder str = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
         while (n > 0) {
-            str.append(n % 3);
+            sb.append(n % 3);
             n /= 3;
         }
         
-        answer = Integer.parseInt(str.toString(), 3);
-               
+        answer = Integer.parseInt(sb.toString(), 3);
         return answer;
     }
 }
