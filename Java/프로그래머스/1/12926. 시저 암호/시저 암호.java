19b7685340875aa144379b4bb5baa692b38a72f4
jangutae <jsilll101389@gmail.com>
2024-12-23 09:09:10 +0900

    [level 1] Title: 시저 암호, Time: 1.40 ms, Memory: 83.8 MB -BaekjoonHub

--- Java/프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ Java/프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,22 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
         char[] chars = s.toCharArray();
         
         for (int i = 0; i < chars.length; i++) {
-            if (Character.isLowerCase(chars[i])) {
-                chars[i] = (char) ((chars[i] - 'a' + n) % 26 + 'a');
-            } else if (Character.isUpperCase(chars[i])) {
-                chars[i] = (char) ((chars[i] - 'A' + n) % 26 + 'A');
-            } else {
+            if (chars[i] == ' ') {
                 chars[i] = ' ';
+            } else if (Character.isUpperCase(chars[i])) {
+                chars[i] = (char) (((int) chars[i] + n - 'A') % 26 + 'A');
+            } else if (Character.isLowerCase(chars[i])) {
+                chars[i] = (char) (((int) chars[i] + n - 'a') % 26 + 'a');
             }
             
         }
         answer = String.valueOf(chars);
-        System.out.println(String.valueOf(chars));
+        
         return answer;
     }
 }
