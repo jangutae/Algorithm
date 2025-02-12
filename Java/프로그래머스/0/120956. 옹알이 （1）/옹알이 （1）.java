e92cb463d2a3aece55c6a835c2bfe540ccdaa31f
jangutae <jsilll101389@gmail.com>
2025-02-12 22:01:32 +0900

    [level 0] Title: 옹알이 (1), Time: 6.21 ms, Memory: 86.8 MB -BaekjoonHub

--- Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,19 +2,14 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] canDo = {"aya", "ye" , "woo", "ma"};
-        
         for (String bab : babbling) {
-            for (String say : canDo) {
-                if (bab.contains(say)) {
-                    bab = bab.replaceAll(say, ",");
-                    if (bab.replaceAll(",", "").isEmpty()) {
-                        answer++;
-                    }
-                }
+            for (String can : canDo) {
+                 if (bab.contains(can)) {
+                     bab = bab.replaceAll(can, ",");
+                      if (bab.replaceAll(",", "").isEmpty()) answer++;
+                     }
+                 }
             }
-        }
-        
-       
         return answer;
+        }
     }
-}
