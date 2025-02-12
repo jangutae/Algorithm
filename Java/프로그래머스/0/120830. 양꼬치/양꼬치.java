71794232f172707ef41c0bb22ec774c87373c29e
jangutae <jsilll101389@gmail.com>
2025-02-12 17:47:10 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ Java/프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
+        // int answer = 0;
         
-        if (n / 10 >= k) {
-            answer = n * 12000;    
-        } else if (n / 10 < k) {
-            answer = n * 12000 + (k - n / 10) * 2000; 
-        }
-        return answer;
+        return n * 12000 + k * 2000 - n / 10 * 2000;
+        // if (n / 10 >= k) {
+        //     answer = n * 12000;    
+        // } else if (n / 10 < k) {
+        //     answer = n * 12000 + (k - n / 10) * 2000; 
+        // }
+        // return answer;
     }
 }
