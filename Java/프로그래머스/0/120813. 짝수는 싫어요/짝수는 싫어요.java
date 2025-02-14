0cbd6bf0abcc4aa10d8c6d4bac8254bb511cc359
jangutae <jsilll101389@gmail.com>
2025-02-14 23:04:54 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

--- Java/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ Java/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,13 +3,19 @@ import java.util.stream.*;
 class Solution {
     public int[] solution(int n) {
         
-        return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).toArray();
-//         int index = 0;
-//         for (int i = n; i >= 0; i--) {
-//             if (i % 2 != 0) index++; 
-//         }
+        // return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).toArray();
+        int index = 0;
+        for (int i = n; i >= 0; i--) {
+            if (i % 2 != 0) index++; 
+        }
         
-//         int[] answer = new int[index];
-//         return answer;
+        int[] answer = new int[index];
+        index = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+              answer[index++] = i;  
+            }  
+        }
+        return answer;
     }
 }
