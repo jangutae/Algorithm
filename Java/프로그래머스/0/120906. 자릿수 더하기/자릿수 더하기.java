3cfb964a140dc3564070c5511e1852c3577963e3
jangutae <jsilll101389@gmail.com>
2025-02-14 10:58:37 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ Java/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,9 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String[] str = String.valueOf(n).split("");
-        for (String s : str) {
-            answer += Integer.parseInt(s);
+//         String[] str = String.valueOf(n).split("");
+        
+//         for (String s : str) {
+//             answer += Integer.parseInt(s);
+//         }
+        
+        while(n > 0) {
+            answer+= n % 10;
+            n /= 10;
         }
         return answer;
     }
