8540a2eb4022dd1a7b4d182a1aad88fd768651cc
jangutae <jsilll101389@gmail.com>
2025-02-14 12:00:46 +0900

    [level 0] Title: 모음 제거, Time: 0.23 ms, Memory: 85.8 MB -BaekjoonHub

--- Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,25 +2,16 @@ import java.util.*;
 
 class Solution {
     public String solution(String myString) {
-        String answer = "";
         
-        answer = myString.replaceAll("[aeiou]", "");
-            
+        String[] strArr = {"a", "e", "i", "o", "u"};
+        
+        for (String s1 : strArr) {
+            if (myString.contains(s1)) {
+                myString = myString.replaceAll(s1, "");
+            }
+        }
         
         
-        
-        
-//         String[] strArr = {"a", "e", "i", "o", "u"};
-//         String[] ms = myString.split("");
-//         StringBuilder sb = new StringBuilder();
-        
-//         for (String s1 : ms) {
-//             if (Arrays.asList(strArr).contains(s1)) {
-//                 continue;
-//             }
-//             answer = sb.append(s1).toString();
-//         }
-        
-        return answer;
+        return myString;
     }
 }
