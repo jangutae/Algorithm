7372ffce5a3a87b8f38d0622417267f3ce34f52b
jangutae <jsilll101389@gmail.com>
2025-02-14 09:30:46 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,12 +2,12 @@ class Solution {
     public String solution(String myString) {
         String answer = "";
         
-        String[] strArray = myString.split("");
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = strArray.length -1; i >= 0; i--) {
-           answer = sb.append(strArray[i]).toString();
-        }
+        StringBuilder sb = new StringBuilder(myString);
+        sb.reverse();
+        answer = sb.toString();
+        // for (int i = strArray.length -1; i >= 0; i--) {
+        //    answer = sb.append(strArray[i]).toString();
+        // }
         
         return answer;
     }
