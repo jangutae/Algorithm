8a41024281d13ce82b177288bc241565137e8167
jangutae <jsilll101389@gmail.com>
2025-02-16 15:11:05 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 88.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ Java/프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,17 +1,18 @@
 class Solution {
     public int solution(int hp) {
-        int answer = (int) hp / 5 + (hp % 5 / 3) + (hp % 5) % 3; 
-        // if (hp % 5 == 0) {
-        //     answer = hp / 5;
-        // } else if (hp % 5 < 3) {
-        //     answer = (int) hp / 5 + hp % 5;
-        // } else if (hp % 5 < 5) {
-        //     if ((hp % 5) % 3 == 0) {
-        //         answer = (int) hp / 5 + 1;
-        //     } else if ((hp % 5) % 3 == 1) {
-        //         answer = (int) hp / 5 + 2
-        //     }
-        // }
+        int answer = 0; 
+        // int answer = (int) hp / 5 + (hp % 5 / 3) + (hp % 5) % 3; 
+        if (hp % 5 == 0) {
+            answer = hp / 5;
+        } else if (hp % 5 < 3) {
+            answer = (int) hp / 5 + hp % 5;
+        } else if (hp % 5 < 5) {
+            if ((hp % 5) % 3 == 0) {
+                answer = (int) hp / 5 + 1;
+            } else if ((hp % 5) % 3 == 1) {
+                answer = (int) hp / 5 + 2;
+            }
+        }
         return answer;
     }
 }
