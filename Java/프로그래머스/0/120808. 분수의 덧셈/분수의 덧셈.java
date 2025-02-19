97b4636cd2b21f78998fa7385a3fc78ece1ce550
jangutae <jsilll101389@gmail.com>
2025-02-19 20:31:01 +0900

    [level 0] Title: 분수의 덧셈, Time: 5.26 ms, Memory: 84.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ Java/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
+        int mother = denom1 * denom2;
+        int totalSon = numer1 * denom2 + numer2 * denom1;
         
-        // 최대 공약수
-        int lcm = denom1 * denom2 / gcd(denom1, denom2);
-        int numerator = numer1 * (lcm / denom1) + numer2 * (lcm / denom2);
-        int result = gcd(lcm, numerator);
-        
-        answer[1] = lcm / result;
-        answer[0] = numerator / result;        
-        
-        return answer;
-    }
-    
-     private int gcd(int a, int b) {
-        while (b != 0) {
-           int temp = b;
-            b = a % b;
-            a = temp;
+        for (int i = mother; i > 0; i--) {
+            if (mother % i == 0 && totalSon % i == 0) {
+                mother /= i;
+                totalSon /= i;
+            }
         }
-        return a;
+        
+        
+        
+        return new int[]{totalSon, mother};
     }
 }
