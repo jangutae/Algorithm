6d6ba2cb1ee6e39681a61a0442d8f7032fbdb08c
jangutae <jsilll101389@gmail.com>
2025-02-21 11:19:02 +0900

    [level 0] Title: 가까운 수, Time: 0.10 ms, Memory: 88.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,43 +1,17 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] array, int n) {
-//         int answer = 0;
-//         int[] clone = array.clone();
+        int answer = array[0];
+        int minClose = Math.abs(array[0] - n);
         
-//         Arrays.sort(clone);
-        
-//         Map<Integer, Integer> map = new HashMap<>();
-        
-//         for (int i = 0; i < array.length; i++) {
-//             int abs = Math.abs(clone[i] - n);
-//             map.put(clone[i], abs);
-//         }
-        
-//         for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
+            int close = Math.abs(array[i] - n);
             
-//             int close = map.get(array[i]);
-//             int min = map.get(array[0]);;
-//             System.out.println(close);
-//             System.out.println(min);
-//             if (close < min || close == min) {
-//                 min = close;
-//             }
-//             answer = array[i];
-//         }
-        int closest = array[0]; // 첫 번째 요소를 초기값으로 설정
-        int minDiff = Math.abs(n - closest); // 초기 최소 차이값
-
-        for (int num : array) {
-            int diff = Math.abs(n - num);
-
-            // 더 작은 차이를 찾거나, 차이가 같다면 더 작은 수를 선택
-            if (diff < minDiff || (diff == minDiff && num < closest)) {
-                closest = num;
-                minDiff = diff;
+            if (close < minClose || minClose == close && array[i] < answer) {
+                answer = array[i];
+                minClose = close;
             }
         }
-        return closest;
-        // return answer;
+  
+        return answer;
     }
 }
