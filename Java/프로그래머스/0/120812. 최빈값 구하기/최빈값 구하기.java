a663d864f5ffb8d801e9ea804d8b80d00f32a38f
jangutae <jsilll101389@gmail.com>
2025-03-02 02:07:34 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.51 ms, Memory: 91.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ Java/프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,20 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        int maxCount = 0;
         int answer = 0;
+        int len = array.length;
+        Arrays.sort(array);
+        
         Map<Integer, Integer> map = new HashMap<>();
-        for(int number : array){
-            int count = map.getOrDefault(number, 0) + 1;
-            if(count > maxCount){
-                maxCount = count;
-                answer = number;
-            }
-            else  if(count == maxCount){
-                answer = -1;
-            }
-            map.put(number, count);
+        
+        for (int num : array) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        
+        int maxFreq = 0; // 최댓값 빈도수 저장 변수
+        int mode = -1; // 최빈값 저장 변수
+        boolean multipleModes = false; // 최빈값이 여러 개인지 여부
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            
+            if (value > maxFreq) {
+                maxFreq = value;
+                mode = key;
+                multipleModes = false;
+            } else if (value == maxFreq) {
+                multipleModes = true;
+            }
+        }
+        
+        answer = multipleModes ? -1 : mode;
+        
+        System.out.println(map);
         return answer;
     }
 }
