d24b68b924bfcd7cb343b7bfbf0bf8ff07663a4c
jangutae <jsilll101389@gmail.com>
2025-03-01 23:26:22 +0900

    [level 0] Title: OX퀴즈, Time: 0.16 ms, Memory: 86.6 MB -BaekjoonHub

--- Java/프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ Java/프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,20 +1,19 @@
 class Solution {
     public String[] solution(String[] quiz) {
-        String[] answer = new String[quiz.length];
-        int sum = 0;
-        String value = "";
+        int len = quiz.length;
+        String[] answer = new String[len];
         
-        for (int i = 0; i < quiz.length; i++) {
+        for (int i = 0; i < len; i++) {
             String[] arr = quiz[i].split(" ");
-            
-           
-            sum = Integer.parseInt(arr[0]) + (Integer.parseInt(arr[2]) * (arr[1].equals("+") ? 1 : -1));
-            value = (Integer.parseInt(arr[4]) == sum) ? "O" : "X"; 
-                
-            
-           answer[i] = value;
+            int value = strToInt(arr[0]) + ((strToInt(arr[2])) * (arr[1].equals("+") ? 1 : -1));
+            String result = strToInt(arr[4]) == value ? "O" : "X";
+            answer[i] = result;
         }
         
         return answer;
+    }
+    
+    private int strToInt(String s) {
+        return Integer.parseInt(s);
     }
 }
