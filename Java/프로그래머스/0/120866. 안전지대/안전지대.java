f4f906f2ab393778456c24859d0c8551d40be7ab
jangutae <jsilll101389@gmail.com>
2025-03-04 11:46:18 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

--- Java/프로그래머스/0/120866. 안전지대/안전지대.java
+++ Java/프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,38 +1,36 @@
 class Solution {
     public int solution(int[][] board) {
-        int n = board.length; // 보드의 크기 (정사각형)
-        int[][] dangerMap = new int[n][n]; // 위험지역 표시할 배열
-        int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0}; // 8방향 (위, 아래, 좌, 우 + 대각선)
+        int answer = 0;
+        int len = board.length;
+        int[][] dangerMap = new int[len][len];
+        int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0}; // 8방향 (대각선 포함)
         int[] dy = {-1, 0, 1, 1, 1, 0, -1, -1};
-
-        // 지뢰(1)가 있는 위치를 찾아서 위험지역 표시
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) { // 지뢰 발견
-                    dangerMap[i][j] = 1; // 자기 자신도 위험지역
-
-                    // 8방향 탐색
-                    for (int d = 0; d < 8; d++) {
+        
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (board[i][j] == 1) {
+                    dangerMap[i][j] = 1;
+                    
+                    for (int d = 0; d < dx.length; d++) {
                         int nx = i + dx[d];
                         int ny = j + dy[d];
 
-                        // 범위 체크 (배열을 벗어나지 않도록)
-                        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                            dangerMap[nx][ny] = 1; // 위험지역 표시
+                        // 배열 경계를 벗어나지 않도록 체크
+                        if (nx >= 0 && nx < len && ny >= 0 && ny < len) {
+                            dangerMap[nx][ny] = 1;
                         }
                     }
+                    
                 }
             }
         }
-
-        // 안전한 지역 개수 세기 (0의 개수)
-        int safeCount = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dangerMap[i][j] == 0) safeCount++;
+        
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (dangerMap[i][j] == 0) answer++;
             }
         }
-
-        return safeCount;
+        
+        return answer;
     }
 }
