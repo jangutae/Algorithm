d2ac7c30c4f003c683acbf463bb15e80829cb545
jangutae <jsilll101389@gmail.com>
2025-03-10 10:01:35 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numList) {
+        int answer = -1;
+        
+        for (int i = 0; i < numList.length; i++) {
+            if (numList[i] < 0) {
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
