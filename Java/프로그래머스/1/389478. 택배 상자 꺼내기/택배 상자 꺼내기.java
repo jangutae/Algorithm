88f02bae8538a707e9cd53e89bbd6fd9996f4905
jangutae <jsilll101389@gmail.com>
2025-03-11 17:40:17 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

--- Java/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ Java/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,48 +1,12 @@
 class Solution {
     public int solution(int n, int w, int num) {
-//         int answer = 0;
+        int answer = 0;
         
-//         // 현재 층수 
-//         int layer = (num % w == 0) ? num / w : num / w + 1 ;
-        
-//         // 전체 층수
-//         int wholeLayer = (n % w == 0) ? n / w : n / w + 1;   
-        
-//         // 층수에서 이동한 칸
-//         int value2 = num % w;
-        
-//         // 꼭대기 층에서 이동한 칸
-//         int value3 = n % w;
-        
-//         if (n % w == 0) {
-//             answer = wholeLayer - layer;
-//         } 
-        
-//         if ( w == 1) {
-//             answer = n - num;
-//         }
-        
-//         while (num + (2 * w) <= wholeLayer * w) {
-        
-//         if (num + (2 * w) > n) {
-//             answer = (wholeLayer - 1) - layer + 1;
-            
-//         } else if (num + (2 * w) <= n) {
-//             answer = wholeLayer - layer + 1;
-//         }
-//         num = num + (2 * w);
-        
-//         }
-        
-        
-//         System.out.println(num);
-//         return answer;
-        
-         int answer = 0;
         while (num <= n) {
             num += 2 * (w - ((num - 1) % w) - 1) + 1;
             answer ++;
         }
+        
         return answer;
     }
 }
