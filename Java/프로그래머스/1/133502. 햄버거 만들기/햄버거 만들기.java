5a6b2dfca7eb30b855c98209f232cedb68799929
jangutae <jsilll101389@gmail.com>
2025-03-12 17:10:02 +0900

    [level 1] Title: 햄버거 만들기, Time: 53.61 ms, Memory: 133 MB -BaekjoonHub

--- Java/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ Java/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,30 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] ingredient) {
-        Stack<Integer> stack = new Stack<>();
-        int count = 0; // 포장된 햄버거 개수
-
-        for (int ing : ingredient) {
-            stack.push(ing); // 재료 추가
-
-            // 스택의 마지막 4개가 [1,2,3,1]인지 확인
-            if (stack.size() >= 4) {
-                int size = stack.size();
-                if (stack.get(size - 4) == 1 && 
-                    stack.get(size - 3) == 2 &&
-                    stack.get(size - 2) == 3 &&
-                    stack.get(size - 1) == 1) {
-                    
-                    // 햄버거 포장 -> 마지막 4개 제거
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-
-                    count++; // 햄버거 개수 증가
-                }
+        int answer = 0;
+        
+        Stack<Integer> makeBurger = new Stack<>();
+        
+        for (int i : ingredient) {
+            makeBurger.push(i);
+        
+        
+        if (makeBurger.size() >= 4) {
+            int size = makeBurger.size();
+            if (
+            makeBurger.get(size - 4) == 1 &&
+            makeBurger.get(size - 3) == 2 &&
+            makeBurger.get(size - 2) == 3 &&
+            makeBurger.get(size - 1) == 1) {
+                makeBurger.pop();
+                makeBurger.pop();
+                makeBurger.pop();
+                makeBurger.pop();
+                
+                answer++;
             }
+            
         }
-        return count;
+        }
+        
+        return answer;
     }
 }
