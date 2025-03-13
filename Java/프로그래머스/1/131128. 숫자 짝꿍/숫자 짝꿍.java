02fb9df30cad907a5acd2a3f86c3022831798d86
jangutae <jsilll101389@gmail.com>
2025-03-13 18:15:28 +0900

    [level 1] Title: 숫자 짝꿍, Time: 82.84 ms, Memory: 162 MB -BaekjoonHub

--- Java/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ Java/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,41 +1,41 @@
 class Solution {
     public String solution(String X, String Y) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
+        
+        // 숫자 0 ~ 9
         
         int[] countX = new int[10];
         int[] countY = new int[10];
         
-        char[] chars1 = X.toCharArray();
-        char[] chars2 = Y.toCharArray();
-        
-        
-        for (char c : chars1) {
-            countX[c - '0']++;    
+        for (char c : X.toCharArray()) {
+            countX[c - '0']++;
         }
         
-        for (char c : chars2) {
-            countY[c - '0']++;    
+        for (char c : Y.toCharArray()) {
+            countY[c - '0']++;
         }
         
+        StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < countX.length; i++) {
+        for (int i = 9; i >= 0; i--) {
             int commonNumber = Math.min(countX[i], countY[i]);
             
-            while (commonNumber-- > 0) {
-                sb.append(i);        
+            while (commonNumber > 0) {
+                sb.append(i);
+                commonNumber--;
             }
         }
         
-        sb.reverse();
-        
         if (sb.length() == 0) {
-            return "-1";
+            answer = "-1";
+        } else if (sb.charAt(0) == '0') {
+            answer = "0";
+        } else {
+            answer = sb.toString();
         }
+    
         
-        if (sb.charAt(0) == '0') {
-            return "0";
-        }
         
-        return sb.toString();
+        return answer;
     }
 }
