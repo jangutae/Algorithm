ddca3772971ccd490cf55c6e5152b8e8a66a10e2
jangutae <jsilll101389@gmail.com>
2025-03-13 19:22:50 +0900

    [level 1] Title: 최소직사각형, Time: 2.05 ms, Memory: 95.5 MB -BaekjoonHub

--- Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,40 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
         int maxX = 0;
-        int maxY = 0;
-        int[][] arr = sizes.clone();
+        int minY = 0;
         
-        for (int i = 0; i < sizes.length; i++) {
-            
-            int valueX = sizes[i][0];
-            int valueY = sizes[i][1];
-            
-            arr[i][0] = valueX > valueY ? valueX : valueY;
-            arr[i][1] = valueX < valueY ? valueX : valueY;
+        for (int[] card : sizes) {
+            maxX = Math.max(maxX, Math.max(card[0], card[1]));
+            minY = Math.max(minY, Math.min(card[0], card[1]));
         }
         
-        for (int i = 0; i < sizes.length; i++) {
-            
-            int valueX = arr[i][0];
-            int valueY = arr[i][1];
-            
-            if (valueX > maxX) {
-                maxX = valueX;
-            }
-            
-            if (valueY > maxY) {
-                maxY = valueY;
-            }
-        }
-        
-        answer = maxX * maxY;
-        
-        
-        System.out.println(maxX);
-        System.out.println(maxY);
+        answer = maxX * minY;
         return answer;
     }
 }
