5a76e87c90151075e73e89854e6eefec48922a12
jangutae <jsilll101389@gmail.com>
2025-03-17 00:22:03 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.78 ms, Memory: 74 MB -BaekjoonHub

--- Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,11 +1,18 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public int solution(int[] num_list) {
-        int sum = IntStream.of(num_list).sum();
-        int multi = IntStream.of(num_list).reduce(1, (a, b) -> a * b);
-        int answer = (multi > sum * sum)? 0 : 1;
+        int sum = 0;
+        int multi = 1;
+        
+        for (int i : num_list) {
+            sum += i;
+            multi *= i;
+        }
+        
+        int answer = Math.pow(sum, 2) > multi ? 1 : 0;
+        
+        System.out.println(multi);
+        System.out.println(Math.pow(sum, 2));
+        
         return answer;
     }
 }
