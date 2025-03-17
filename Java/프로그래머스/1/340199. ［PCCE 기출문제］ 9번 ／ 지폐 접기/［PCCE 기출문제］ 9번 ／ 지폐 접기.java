0f76f2758e33c5d6552260f54a0313f5eb499a23
jangutae <jsilll101389@gmail.com>
2025-03-17 10:56:33 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

--- Java/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ Java/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,35 +1,52 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
-        int answer = 0;
-        int x = bill[0];
-        int y = bill[1];
+//         int answer = 0;
+//         int x = bill[0];
+//         int y = bill[1];
         
-        int result = 0;
-        int b = bill[0];
-        int a = bill[1];
+//         int result = 0;
+//         int b = bill[0];
+//         int a = bill[1];
         
-        while (wallet[0] < x || wallet[1] < y) {
-            if (x > y) {
-                x /= 2;
-            } else {
-                y /= 2;
-            }
+//         while (wallet[0] < x || wallet[1] < y) {
+//             if (x > y) {
+//                 x /= 2;
+//             } else {
+//                 y /= 2;
+//             }
             
+//             answer++;
+//         }
+        
+//         while (wallet[0] < a || wallet[1] < b) {
+//             if (a > b) {
+//                 a /= 2;
+//             } else {
+//                 b /= 2;
+//             }
+            
+//             result++;
+//         }
+        
+        
+//         answer = answer > result ? result : answer;
+//         return answer;
+        int answer = 0;
+
+        while(true) {
+            if((bill[0] <= wallet[0] && bill[1] <= wallet[1]) || 
+               (bill[0] <= wallet[1] && bill[1] <= wallet[0])) break;
+
+            if(bill[0] > bill[1]) {
+                bill[0] /= 2;
+            } else {
+                bill[1] /= 2;
+            }
             answer++;
         }
-        
-        while (wallet[0] < a || wallet[1] < b) {
-            if (a > b) {
-                a /= 2;
-            } else {
-                b /= 2;
-            }
-            
-            result++;
-        }
-        
-        
-        answer = answer > result ? result : answer;
+
         return answer;
+    
+
     }
 }
