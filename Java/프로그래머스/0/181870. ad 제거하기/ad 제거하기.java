6533321ce243a2de8af03181bd65a1fcbfecdfb9
jangutae <jsilll101389@gmail.com>
2025-03-17 09:44:34 +0900

    [level 0] Title: ad 제거하기, Time: 5.98 ms, Memory: 88.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ Java/프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String[] solution(String[] strArr) {
@@ -9,10 +10,13 @@ class Solution {
         }
         
         String[] answer = new String[cnt];
+        // String[] answer = Arrays.stream(strArr).filter(s -> !s.contains("ad")).toArray(String[]::new);
         int index = 0;
         for (int i = 0; i < len; i++) {
             if (!strArr[i].contains("ad")) answer[index++] = strArr[i];
         }
+        
+        
         return answer;
     }
 }
