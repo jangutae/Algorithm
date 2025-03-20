0418881a70632e5be145b7eb344192b03221e1a8
jangutae <jsilll101389@gmail.com>
2025-03-20 11:35:30 +0900

    [level 1] Title: 체육복, Time: 0.16 ms, Memory: 71.9 MB -BaekjoonHub

--- Java/프로그래머스/1/42862. 체육복/체육복.java
+++ Java/프로그래머스/1/42862. 체육복/체육복.java
@@ -1,25 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] people = new int[n];
         int answer = n;
-
-        for (int l : lost) 
-            people[l-1]--;
-        for (int r : reserve) 
-            people[r-1]++;
-
-        for (int i = 0; i < people.length; i++) {
-            if(people[i] == -1) {
-                if(i-1>=0 && people[i-1] == 1) {
-                    people[i]++;
-                    people[i-1]--;
-                }else if(i+1< people.length && people[i+1] == 1) {
-                    people[i]++;
-                    people[i+1]--;
-                }else 
-                    answer--;
+        
+        // 읽어버린 체육복 수량 set
+        Set<Integer> lostSet = new HashSet<>();
+        
+        
+        // 여분을 가지고 있는 수량 set
+        Set<Integer> reserveSet = new HashSet<>();
+        
+        for (int i : lost) {
+            lostSet.add(i);
+        }
+        
+        for (int r : reserve) {
+            if (lostSet.contains(r)) {
+                lostSet.remove(r);
+            } else {
+                reserveSet.add(r);
+            }
+                
+        }
+        
+        for (int set : reserveSet) {
+            if (lostSet.contains(set - 1)) {
+                lostSet.remove(set - 1);
+            } else if (lostSet.contains(set + 1)) {
+                lostSet.remove(set + 1);
             }
         }
-        return answer;
+        
+        return answer - lostSet.size();
     }
 }
