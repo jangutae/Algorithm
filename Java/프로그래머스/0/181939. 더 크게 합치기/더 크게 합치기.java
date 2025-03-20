601eb524b85065f6449f49ba67b9fd1e7753e138
jangutae <jsilll101389@gmail.com>
2025-03-20 09:34:04 +0900

    [level 0] Title: 더 크게 합치기, Time: 22.19 ms, Memory: 75.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(int a, int b) {
-        StringBuilder sb1 = new StringBuilder();
-        sb1.append(b);
-        sb1.append(a);
-        
-        StringBuilder sb2 = new StringBuilder();
-        sb2.append(a);
-        sb2.append(b);
-        int x = Integer.parseInt(sb1.toString());
-        int y = Integer.parseInt(sb2.toString());
-        
-        int answer = x >= y ? x : y;
+        int answer = Math.max(Integer.parseInt("" + a + b), Integer.parseInt("" + b + a));
         return answer;
     }
 }
