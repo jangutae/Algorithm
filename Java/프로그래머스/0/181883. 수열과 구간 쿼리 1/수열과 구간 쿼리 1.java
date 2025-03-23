4dc14ef4a53c4c5553589863730b8ab24e483649
jangutae <jsilll101389@gmail.com>
2025-03-23 12:44:33 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.09 ms, Memory: 73.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ Java/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -3,15 +3,12 @@ class Solution {
         int[] answer = arr.clone();
         
         for (int i = 0; i < queries.length; i++) {
-            int start = queries[i][0]; // 시작 인덱스
-            int end = queries[i][1];   // 끝 인덱스
-            
-            for (int j = start; j <= end; j++) {
-                if (j >= 0 && j < answer.length) { // 배열 범위 체크
-                    answer[j]++;
-                }
+           
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                answer[j]++;
             }
         }
+        
         
         return answer;
     }
